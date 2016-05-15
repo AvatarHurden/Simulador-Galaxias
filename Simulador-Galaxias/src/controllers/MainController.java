fed9a3b9ee4bc70aba2ed3ffd25bfb5a8f07a035
Arthur Vedana <agieselvedana@gmail.com>
2016-05-15 16:47:30 -0300

    Mostra grade, opção para ligar ou desligar grade e vetores

--- Simulador-Galaxias/src/controllers/MainController.java
+++ Simulador-Galaxias/src/controllers/MainController.java
@@ -5,9 +5,11 @@ import java.util.Random;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Slider;
@@ -39,8 +41,11 @@ public class MainController {
 	@FXML private Canvas canvas;
 	
 	@FXML private Slider zoomSlider;
+	
 	private double zoom = 1;
 	private double dragLastX, dragLastY, dragAmountX, dragAmountY;
+	
+	private boolean showGrid = true, showVectors = true, showGravity;
 	
 	@FXML
 	private void initialize() {
@@ -108,11 +113,15 @@ public class MainController {
 		canvas.getGraphicsContext2D().translate(canvas.getWidth() / 2, canvas.getHeight() / 2);
 		
 		canvas.heightProperty().addListener((observable, oldValue, newValue) -> {
-			canvas.getGraphicsContext2D().translate(0, newValue.doubleValue() / 2 - oldValue.doubleValue() / 2);
+			Affine t = new Affine();
+			t.setTy((newValue.doubleValue() / 2 - oldValue.doubleValue() / 2) / zoom);
+			canvas.getGraphicsContext2D().transform(t);
 			drawParticles();
 		});
 		canvas.widthProperty().addListener((observable, oldValue, newValue) -> {
-			canvas.getGraphicsContext2D().translate(newValue.doubleValue() / 2 - oldValue.doubleValue() / 2, 0);
+			Affine t = new Affine();
+			t.setTx((newValue.doubleValue() / 2 - oldValue.doubleValue() / 2) / zoom);
+			canvas.getGraphicsContext2D().transform(t);
 			drawParticles();
 		});
 		
@@ -189,56 +198,89 @@ public class MainController {
 		
 		double width = canvas.getWidth();
 		double height = canvas.getHeight();
+
+		double leftLimit = (-width / 2 - dragAmountX) / zoom;
+		double topLimit = (-height / 2 - dragAmountY) / zoom;
+
+		gc.clearRect(leftLimit, topLimit, width / zoom, height / zoom);
+		
+		if (showGrid)
+			drawGrid();
+		
+		for (Particle p : particles) {
+			gc.setFill(p.getColor());
+			gc.setStroke(p.getColor());
+			double radius = 1 + p.getMass() / 100;
+			gc.fillOval(p.getPositionX() - radius / 2, p.getPositionY() - radius / 2, radius, radius);
+			
+			if (showVectors)
+				drawVelocityVector(gc, p);
+		}
+		
+	}
+	
+	private void drawGrid() {
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		double width = canvas.getWidth();
+		double height = canvas.getHeight();
 		
 		double leftLimit = (-width / 2 - dragAmountX) / zoom;
 		double rightLimit = (width / 2 - dragAmountX) / zoom;
 		double bottomLimit = (height / 2 - dragAmountY) / zoom;
 		double topLimit = (-height / 2 - dragAmountY) / zoom;
-
-		gc.clearRect(leftLimit, topLimit, width / zoom, height / zoom);
 		
 		gc.setStroke(Paint.valueOf("#303050"));
-		gc.strokeLine(0, topLimit, 0, bottomLimit);
-		gc.strokeLine(leftLimit, 0, rightLimit, 0);
-		
-		for (Particle p : particles) {
-			gc.setFill(p.getColor());
-			gc.setStroke(p.getColor());
-			double radius = 5 + p.getMass() / 100;
-			gc.fillOval(p.getPositionX() - radius / 2, p.getPositionY() - radius / 2, radius, radius);
-			
-			double x1 = p.getPositionX(), x2 = x1 + p.getVelocityX();
-			double y1 = p.getPositionY(), y2 = y1 + p.getVelocityY();
-			
-			double phi= Math.toRadians(40);
-			double barb = 1;
-			double dx = x2 - x1;
-            double dy = y2 - y1;
-            double theta = Math.atan2( dy, dx );
-            double x, y, rho = theta + phi;
-
-		    gc.setLineWidth(0.5);
-		    gc.setLineCap(StrokeLineCap.ROUND);
-		    gc.strokeLine(p.getPositionX(), p.getPositionY(), p.getPositionX() + p.getVelocityX(), 
-		    		p.getPositionY() + p.getVelocityY());
-
-            x = x2 - barb * Math.cos( rho );
-            y = y2 - barb * Math.sin( rho );
-            gc.strokeLine(x2, y2, x, y);
-            rho = theta - phi;
-            x = x2 - barb * Math.cos( rho );
-            y = y2 - barb * Math.sin( rho );
-            gc.strokeLine(x2, y2, x, y);
-		    gc.setLineWidth(1);
-			
+		gc.setLineWidth(1 / zoom);
+		int distance = 300;
+		int divider = (int) width / 80;
+		while (distance > width / zoom / divider)
+			distance /= 2;
+		int lines = (int) width / distance * divider;
+		for (int i = -lines; i < lines; i++) {
+			double amount = i * distance;
+			gc.strokeLine(amount, topLimit, amount, bottomLimit);
+			gc.strokeLine(leftLimit, amount, rightLimit, amount);
 		}
 		
 	}
 	
+	private void drawVelocityVector(GraphicsContext gc, Particle p) {
+		double radius = 1 + p.getMass() / 100;
+
+		double x1 = p.getPositionX(), x2 = x1 + p.getVelocityX();
+		double y1 = p.getPositionY(), y2 = y1 + p.getVelocityY();
+
+	    gc.setLineWidth(0.1 * radius);
+	    gc.setLineCap(StrokeLineCap.ROUND);
+	    gc.strokeLine(x1, y1, x2, y2);
+	    
+	    if (p.getVelocityX() == 0 && p.getVelocityY() == 0) {
+		    gc.setLineWidth(1);
+			return;
+	    }
+
+		double phi= Math.toRadians(40);
+		double barb = 1;
+		double dx = x2 - x1;
+        double dy = y2 - y1;
+        double theta = Math.atan2( dy, dx );
+        double x, y, rho = theta + phi;
+        
+        x = x2 - barb * Math.cos( rho );
+        y = y2 - barb * Math.sin( rho );
+        gc.strokeLine(x2, y2, x, y);
+        rho = theta - phi;
+        x = x2 - barb * Math.cos( rho );
+        y = y2 - barb * Math.sin( rho );
+        gc.strokeLine(x2, y2, x, y);
+	    gc.setLineWidth(1);
+	    
+	}
 	
 	@FXML
-	private void mouseEnteredCanvas() {
-		System.out.println("hello");
+	private void mouseMovedCanvas(MouseEvent evt) {
+		
 	}
 	
 	@FXML
@@ -256,7 +298,6 @@ public class MainController {
 	private void mouseDraggedCanvas(MouseEvent evt) {
 		dragAmountX += evt.getX() - dragLastX;
 		dragAmountY += evt.getY() - dragLastY;
-		
 
 		Affine t = new Affine();
 		t.setTx((evt.getX() - dragLastX) / zoom);
@@ -270,27 +311,7 @@ public class MainController {
 	
 	@FXML
 	private void scrolledCanvas(ScrollEvent evt) {
-
 		zoomSlider.setValue(zoomSlider.getValue() + evt.getDeltaY() / 40);
-		
-//		double thisZoom = 1;
-//		if (evt.getDeltaY() > 0)
-//			thisZoom *= evt.getDeltaY() / 30;
-//		else
-//			thisZoom /= -evt.getDeltaY() / 30;
-//		
-//		if ((zoom < 0.1 && thisZoom < 1) || 
-//			(zoom > 10 && thisZoom > 1))
-//			thisZoom = 1;
-//		zoom *= thisZoom;
-//		
-//		
-//		Affine t = new Affine();
-//		t.setMxx(thisZoom);
-//		t.setMyy(thisZoom);
-//		canvas.getGraphicsContext2D().transform(t);
-//		
-//		drawParticles();
 	}
 	
 	@FXML
@@ -344,6 +365,20 @@ public class MainController {
 	@FXML 
 	private void startSimulation() {
 		
+	}
+	
+	@FXML
+	private void toggleGridVisibility(ActionEvent evt) {
+		CheckMenuItem source = (CheckMenuItem) evt.getSource();
+		showGrid = source.isSelected();
+		drawParticles();
+	}
+	
+	@FXML
+	private void toggleVectorVisibility(ActionEvent evt) {
+		CheckMenuItem source = (CheckMenuItem) evt.getSource();
+		showVectors = source.isSelected();
+		drawParticles();
 	}
 	
 }
